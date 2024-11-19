76ffdafda1836ab1d0c46cfacbfea80a16effe9f
Nathan Chee <nkc8923@hawaii.edu>
2024-11-18 21:59:03 -1000

    done

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,10 +1,10 @@
 import { getServerSession } from 'next-auth';
 import { Col, Container, Row } from 'react-bootstrap';
-import { prisma } from '@/lib/prisma';
 import { loggedInProtectedPage } from '@/lib/page-protection';
 import authOptions from '@/lib/authOptions';
 import ClubCard from '@/components/ClubCard';
 import { Club } from '@prisma/client';
+import { prisma } from '@/lib/prisma';
 
 /** Render a list of stuff for the logged in user. */
 const ListPage = async () => {
