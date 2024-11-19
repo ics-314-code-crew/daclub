25f44795c8f7cb48e00957fa4d8988fd45596aa7
Nathan Chee <nkc8923@hawaii.edu>
2024-11-18 20:39:35 -1000

    fixed some of it

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,31 +3,32 @@ import { hash } from 'bcrypt';
 import * as config from '../config/settings.development.json';
 
 const prisma = new PrismaClient();
-
 async function main() {
   console.log('Seeding the database');
   const password = await hash('changeme', 10);
 
-  for (const account of config.defaultAccounts) {
+  config.defaultAccounts.forEach(async (account) => {
     let role: Role = 'USER';
-    if (account.role === 'SUPER_ADMIN') 
-      role = 'SUPER_ADMIN';
-    else if (account.role === 'CLUB_ADMIN') 
+    if (account.role === 'CLUB_ADMIN') {
       role = 'CLUB_ADMIN';
-
+    } else if (account.role === 'SUPER_ADMIN') {
+      role = 'SUPER_ADMIN';
+    }
     console.log(`Creating user: ${account.email} with role: ${role}`);
     await prisma.user.upsert({
       where: { email: account.email },
       update: {},
       create: {
+        firstName: account.firstName,
+        lastName: account.lastName,
         email: account.email,
         password,
         role,
       },
     });
-  }
+  });
 
-  for (const interest of config.defaultInterests) {
+  config.defaultInterests.forEach(async (interest) => {
     console.log(`Creating interest: ${interest.name}`);
     await prisma.interest.upsert({
       where: { name: interest.name },
@@ -36,29 +37,28 @@ async function main() {
         name: interest.name,
       },
     });
-  }
-  
+  });
 
-  for (const data of config.defaultClubsData) {
+  config.defaultClubsData.forEach(async (data) => {
     console.log(`Adding club: ${data.name}`);
-  
+
     // Ensure categories exist
     const categories = await prisma.interest.findMany({
       where: { name: { in: data.categories } },
     });
-  
+
     // Ensure admins exist
     const admins = await prisma.user.findMany({
       where: { email: { in: data.admins } },
     });
-  
+
     if (categories.length !== data.categories.length || admins.length !== data.admins.length) {
-      console.warn(
-        `Skipping club: ${data.name} due to missing categories or admins.`
-      );
-      continue;
+      console.warn(`Skipping club: ${data.name} due to missing categories or admins.`);
     }
-  
+  });
+
+  config.defaultClubsData.forEach(async (data) => {
+    console.log(`Adding club: ${data.name}`);
     await prisma.club.upsert({
       where: { name: data.name },
       update: {},
@@ -79,7 +79,7 @@ async function main() {
         },
       },
     });
-  }  
+  });
 }
 
 main()
