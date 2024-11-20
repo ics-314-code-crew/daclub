83af2c5aa03dda1d28fd53f98aaf6fbc1981024f
edenkp <edenkp@hawaii.edu>
2024-11-20 12:26:17 -1000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,9 @@ const Home = () => (
           <h1>Welcome to Da Club!</h1>
           <h5>
             This website allows students to explore and learn about the different clubs offered at UH Manoa!
-            Students can find an assortment of clubs ranging from athletics to art and so much more.
-            We hope this website will help you find a club that interests you and that you will enjoy!
+            Students can find an assortment of clubs ranging from athletics to art, technology, and so much more.
+            Da Club also helps clubs to recruit new members by advertising their clubs using this website.
+            We hope you enjoy your time here and find a club that interests you!
           </h5>
         </Col>
       </Row>
