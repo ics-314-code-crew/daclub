9a807012763b5fb6aa3ef1859d6d2be55d93e4cd
edenkp <edenkp@hawaii.edu>
2024-11-20 12:44:43 -1000

    updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,13 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import './globals.css';
+import MiddleMenu from '@/components/MiddleMenu';
 
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" className="py-3">
-      <Row className="align-middle text-center">
-        <Col>
-          <h1>Welcome to Da Club!</h1>
-          <h5>
-            This website allows students to explore and learn about the different clubs offered at UH Manoa!
-            Students can find an assortment of clubs ranging from athletics to art, technology, and so much more.
-            Da Club also helps clubs to recruit new members by advertising their clubs using this website.
-            We hope you enjoy your time here and find a club that interests you!
-          </h5>
-        </Col>
-      </Row>
+    <Container fluid id="footer">
+      <MiddleMenu />
     </Container>
   </main>
 );
