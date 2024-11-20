2d281a5db7f7921a458a45aff4940aaf278f0937
Jayden Sagayaga <jkes@hawaii.edu>
2024-11-20 01:41:18 -1000

    ClubPage update

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -2,14 +2,17 @@
 
 import { Club } from '@prisma/client';
 import { Card, Image } from 'react-bootstrap';
+import  Link  from 'next/link';
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
 const ClubCard = ({ club }: { club: Club }) => (
+  <Link href={`/club/${club.id}`}>
   <Card className="h-100 text-center">
     <Image src={club.logo} width={100} className="club-image" />
     <Card.Body>
       <Card.Title>{club.name}</Card.Title>
     </Card.Body>
   </Card>
+  </Link>
 );
 
 export default ClubCard;
