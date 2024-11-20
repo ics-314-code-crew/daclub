a31e7bae5aac7ab56947f3e7e5f24563b9b38018
Nathan Chee <nkc8923@hawaii.edu>
2024-11-20 00:41:23 -1000

    UPDATED

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -5,7 +5,7 @@ import { Card, Image } from 'react-bootstrap';
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
 const ClubCard = ({ club }: { club: Club }) => (
   <Card className="h-100 text-center">
-    <Image src={club.logo} className="club-image" />
+    <Image src={club.logo} width={100} className="club-image" />
     <Card.Body>
       <Card.Title>{club.name}</Card.Title>
     </Card.Body>
