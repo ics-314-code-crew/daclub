a0900421553d348814fc8fbf1d15925544397277
Jake Dickinson <92128947+jaked332@users.noreply.github.com>
2024-11-19 20:15:44 -1000

    Add base validation schemas

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,24 +1,46 @@
 import * as Yup from 'yup';
 
-export const AddStuffSchema = Yup.object({
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
+export const CreateUserSchema = Yup.object({
+  credentials: Yup.object({
+    email: Yup.string().email('Invalid email').required(),
+    password: Yup.string()
+      .min(6, 'Must be at least 6 characters').required(),
+  }),
+  user: Yup.object({
+    firstName: Yup.string().required(),
+    lastName: Yup.string().required(),
+    email: Yup.string().email('Invalid email').required(),
+  }),
 });
 
-export const EditStuffSchema = Yup.object({
+export const EditUserSchema = Yup.object({
+  id: Yup.number().required(),
+  email: Yup.string().email('Invalid email').required(),
+  role: Yup.mixed().oneOf(
+    ['USER', 'CLUB_ADMIN', 'SUPER_ADMIN'],
+    'Invalid role',
+  ),
+});
+
+export const CreateClubSchema = Yup.object({
+  name: Yup.string().required(),
+  description: Yup.string().required(),
+  meetingTime: Yup.string().required(),
+  location: Yup.string().required(),
+  website: Yup.string().url(),
+  contactEmail: Yup.string().email(),
+  photos: Yup.array().of(Yup.string().url()),
+  expiration: Yup.date().required(),
+});
+
+export const EditClubSchema = Yup.object({
   id: Yup.number().required(),
   name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-});
-
-export const AddClubSchema = Yup.object({
-  name: Yup.string().required(),
-  image: Yup.string().url().required(),
   description: Yup.string().required(),
-  email: Yup.string().email().required(),
-  owner: Yup.string().required(),
+  meetingTime: Yup.string().required(),
+  location: Yup.string().required(),
+  website: Yup.string().url(),
+  contactEmail: Yup.string().email(),
+  photos: Yup.array().of(Yup.string().url()),
+  expiration: Yup.date().required(),
 });
