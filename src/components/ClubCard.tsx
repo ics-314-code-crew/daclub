49c40f9410e3f2341c546d08d405cc012facc02c
Jayden Sagayaga <jkes@hawaii.edu>
2024-11-20 02:12:14 -1000

    ClubPage updates

--- src/components/ClubCard.tsx
+++ src/components/ClubCard.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
 const ClubCard = ({ club }: { club: Club }) => (
-  <Link href={`/club/${club.id}`}>
+  <Link href={`pages/clubs/${club.id}`} passHref>
     <Card className="h-100 text-center">
       <Image src={club.logo} width={100} className="club-image" />
       <Card.Body>
