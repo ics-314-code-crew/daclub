4aa245303252e241c2e99f99026066cdf0b0881a
edenkp <edenkp@hawaii.edu>
2024-11-20 20:45:54 -1000

    Update MiddleMenu.tsx

--- src/components/MiddleMenu.tsx
+++ src/components/MiddleMenu.tsx
@@ -20,8 +20,8 @@ const MiddleMenu = () => (
           This website allows students to explore and learn about the different clubs offered at UH Manoa!
           Students can find an assortment of clubs ranging from athletics to art, technology, and so much more.
           Da Club also helps clubs to recruit new members by advertising their clubs using this website.
-          We hope you enjoy your time here and find a club that interests you!
         </h5>
+        <h5>We hope you enjoy your time here and find a club that interests you!</h5>
       </div>
     </Col>
   </Row>
